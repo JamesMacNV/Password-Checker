fa8bc84875804012c5eb9fc720abc07d2a6aff3f
JamesMacNV <36140375+JamesMacNV@users.noreply.github.com>
2020-01-05 17:56:43 -0800

    Working

--- passwordchecker/checkmypass.py
+++ passwordchecker/checkmypass.py
@@ -29,7 +29,8 @@ def main(args):
         if count:
             print(f'{password} was found {count} times... you should probably change your password!')
         else:
-            print(f'{password} was NOT found. Carry on')
-        return 'done!'        
+            print(f'{password} was NOT found. Carry on')    
+    return 'done!'
 
-main(sys.argv[1:])
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
